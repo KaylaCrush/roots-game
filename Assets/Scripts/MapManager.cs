c7685a198fc761cba64b5c6f420985b8797fea70
Sinnombre <sinnombre124@gmail.com>
2023-02-04 13:26:10 -0800

    tiles!

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    [SerializeField]
+    private List<TileData> tileDatas;
+
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    private void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach (var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            float Toughness = dataFromTiles[clickedTile].Toughness;
+
+            print("At position " + gridPosition + " there is a " + clickedTile + " with toughness " + Toughness);
+
+        }
+    }
+
+    public float GetTileToughness(Vector2 worldPosition)
+    {
+        Vector3Int gridPosition = map.WorldToCell(worldPosition);
+        TileBase tile = map.GetTile(gridPosition);
+        
+        if (tile == null)
+            return 1f;
+
+        float Toughness = dataFromTiles[tile].Toughness;
+        return Toughness;
+    }
+
+    public float GetTileNutrientGatherSpeed(Vector2 worldPosition)
+    {
+        Vector3Int gridPosition = map.WorldToCell(worldPosition);
+        TileBase tile = map.GetTile(gridPosition);
+
+        if (tile == null)
+            return 1f;
+
+        float NutrientGatherSpeed = dataFromTiles[tile].NutrientGatherSpeed;
+        return NutrientGatherSpeed;
+    }
+
+}
